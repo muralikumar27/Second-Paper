5f4b1c4cd0f85fa00a56185bf91c6fb6fae9ca2b
muralikumar27 <muralikumar62345@gmail.com>
2025-01-20 23:52:54 +0530

    First commit

---
+++ Backend/secondpaper/src/main/java/com/murali/secondpaper/SecondPaperApplication.java
@@ -0,0 +1,13 @@
+package com.murali.secondpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondPaperApplication.class, args);
+	}
+
+}
